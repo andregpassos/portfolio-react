1454c16f3cb63e0b55e52600db3a8bc727c310a4
F0738363 André Gomes Passos <andre.passos@bb.com.br>
2022-09-01 15:10:10 -0300

    Cria header e home banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import "./App.css";
+import "./App.scss";
+import profilePicture from "./assets/images/wolf_head.png";
 
 function App() {
-  return <h1>Esqueleto Inicial</h1>;
+  return (
+    <>
+      <header className="header">
+        <a className="headerLink" href="/">
+          Works
+        </a>
+        <a className="headerLink" href="/">
+          Blog
+        </a>
+        <a className="headerLink" href="/">
+          Contact
+        </a>
+      </header>
+      <div className="containerPrincipal">
+        <div className="containerInterno">
+          <div>
+            <h1 className="introText">Hi, I am Wolf, Creative Technologist</h1>
+            <p className="descriptionResume">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae dolor amet ab aliquid. Minima, unde! Illum maxime odio
+              quae porro commodi! Aspernatur repudiandae illo ex sed officiis
+              adipisci incidunt animi?
+            </p>
+            <button className="btnDownloadResume">Download Resume</button>
+          </div>
+          <div>
+            <img
+              className="profilePicture"
+              src={profilePicture}
+              alt="Wolf Head"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
