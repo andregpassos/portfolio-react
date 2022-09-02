f96fbeec31add1cfd94542837c408134199797be
F0738363 André Gomes Passos <andre.passos@bb.com.br>
2022-09-02 15:30:28 -0300

    Cria recent posts e featured works

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./App.scss";
 import profileImage from "./assets/images/wolf_head.png";
+import dashboardImg from "./assets/images/dashboard.png";
+import womanImg from "./assets/images/woman.png";
+import charCursiveImg from "./assets/images/char_cursive.png";
 
 function App() {
   return (
@@ -22,8 +25,8 @@ function App() {
             <p className="descriptionResume">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Repudiandae dolor amet ab aliquid. Minima, unde! Illum maxime odio
-              quae porro commodi! Aspernatur repudiandae illo ex sed officiis
-              adipisci incidunt animi?
+              clear quae porro commodi! Aspernatur repudiandae illo ex sed
+              officiis adipisci incidunt animi?
             </p>
             <button className="btnDownloadResume">Download Resume</button>
           </div>
@@ -39,12 +42,99 @@ function App() {
                 View all
               </a>
             </div>
-            <p className="descriptionResume">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Repudiandae dolor amet ab aliquid. Minima, unde! Illum maxime odio
-              quae porro commodi! Aspernatur repudiandae illo ex sed officiis
-              adipisci incidunt animi?
-            </p>
+            <div className="cardsContainer">
+              <div className="card">
+                <h3 className="titleCard">
+                  Making a design system from scratch
+                </h3>
+                <div className="textCard">
+                  <p className="dateCard">02 Sep 2022</p>
+                  <p className="dateCard">|</p>
+                  <p className="dateCard">Design, Pattern</p>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+                  voluptatum asperiores voluptatem facere impedit sunt,
+                  similique ut voluptas, rerum nam, quisquam quos maiores
+                  tempore nesciunt cupiditate. Praesentium sequi error quam?
+                </p>
+              </div>
+              <div className="card">
+                <h3 className="titleCard">
+                  Making a design system from scratch
+                </h3>
+                <div className="textCard">
+                  <p className="dateCard">02 Sep 2022</p>
+                  <p className="dateCard">|</p>
+                  <p className="dateCard">Design, Pattern</p>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+                  voluptatum asperiores voluptatem facere impedit sunt,
+                  similique ut voluptas, rerum nam, quisquam quos maiores
+                  tempore nesciunt cupiditate. Praesentium sequi error quam?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="featuredWorksContainer">
+          <h3 className="textRecentPosts">Featured works</h3>
+          <div className="featuredWorkCard">
+            <img className="dashboardImg" src={dashboardImg} />
+            <div>
+              <h1 className="titleFeatWorkCard">Designing Dashboards</h1>
+              <div style={{ display: "flex", marginTop: "20px" }}>
+                <p className="yearCard">2020</p>
+                <p style={{ marginLeft: "20px", color: "#828282" }}>
+                  Dashboard
+                </p>
+              </div>
+              <p style={{ marginTop: "20px" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eum soluta exercitationem quasi sed. Odit, sunt! Tempore velit
+                dicta voluptate, aut magnam non culpa temporibus aliquid
+                reiciendis sunt alias ab?
+              </p>
+            </div>
+          </div>
+          <div className="line"></div>
+          <div className="featuredWorkCard">
+            <img className="dashboardImg" src={dashboardImg} />
+            <div>
+              <h1 className="titleFeatWorkCard">Designing Dashboards</h1>
+              <div style={{ display: "flex", marginTop: "20px" }}>
+                <p className="yearCard">2020</p>
+                <p style={{ marginLeft: "20px", color: "#828282" }}>
+                  Dashboard
+                </p>
+              </div>
+              <p style={{ marginTop: "20px" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eum soluta exercitationem quasi sed. Odit, sunt! Tempore velit
+                dicta voluptate, aut magnam non culpa temporibus aliquid
+                reiciendis sunt alias ab?
+              </p>
+            </div>
+          </div>
+          <div className="line"></div>
+          <div className="featuredWorkCard">
+            <img className="dashboardImg" src={dashboardImg} />
+            <div>
+              <h1 className="titleFeatWorkCard">Designing Dashboards</h1>
+              <div style={{ display: "flex", marginTop: "20px" }}>
+                <p className="yearCard">2020</p>
+                <p style={{ marginLeft: "20px", color: "#828282" }}>
+                  Dashboard
+                </p>
+              </div>
+              <p style={{ marginTop: "20px" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eum soluta exercitationem quasi sed. Odit, sunt! Tempore velit
+                dicta voluptate, aut magnam non culpa temporibus aliquid
+                reiciendis sunt alias ab?
+              </p>
+            </div>
           </div>
         </div>
       </div>
